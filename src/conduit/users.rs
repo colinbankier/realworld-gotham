097156c6837d2068b144887dd40467c4a05a9d57
colinbankier <colinbankier@gmail.com>
2019-03-19 17:54:05 +1000

    Setup testing of futures.

--- src/conduit/users.rs
+++ src/conduit/users.rs
@@ -19,23 +19,42 @@ pub fn insert(
     })
 }
 
+pub fn find(
+    repo: Repo,
+    user_id: i32,
+) -> impl Future<Item = Result<User, Error>, Error = BlockingError> {
+    use crate::schema::users::dsl::*;
+    repo.run(move |conn| users.find(user_id).first(&conn))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::test_helpers::generate;
     use fake::fake;
-    use tokio_async_await_test::async_test;
+    use tokio_threadpool::ThreadPool;
 
-    #[async_test]
-    async fn test_create_user() {
+    #[test]
+    fn test_create_user() {
+        let pool = ThreadPool::new();
         let repo = Repo::new();
 
         let new_user = generate::new_user();
-        let user = await! { insert(repo.clone(), new_user) }.expect("Create user failed.");
-
-        let results = await! {
-           find(repo.clone(), user.id)
-        };
+        let future = insert(repo.clone(), new_user).and_then(move |res| {
+            let user = res.expect("Failed to insert user");
+            find(repo.clone(), user.id)
+        });
+        let results = wait_for(&pool, future);
         assert!(results.is_ok());
+    }
+
+    fn wait_for<T>(
+        pool: &ThreadPool,
+        future: impl Future<Item = T, Error = BlockingError> + Send + 'static,
+    ) -> T
+    where
+        T: Send + 'static,
+    {
+        pool.spawn_handle(future).wait().unwrap()
     }
 }
